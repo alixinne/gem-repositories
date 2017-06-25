0ab003a2b9f3bc66ba5d91e971cecfb04df70e8d
Vincent Tavernier <vince.tavernier@gmail.com>
2017-06-25 20:26:57 +0200

    Fix gitlab

--- lib/repositories/hosts/gitlab.rb
+++ lib/repositories/hosts/gitlab.rb
@@ -12,45 +12,55 @@ module Repositories
     class Gitlab < Base
       def initialize(config)
         super(config)
+      end
 
+      def repositories
+        with_gitlab do
+          ::Gitlab.projects.auto_paginate.collect do |repo|
+            next if self.exclude.include? repo.name
+            next if repo.owner.nil? || @user_id != repo.owner.id
+
+            r = Repository.new(repo.name, repo, repo.ssh_url_to_repo, self)
+
+            ::Gitlab.branches(repo.id).auto_paginate.each do |bran|
+              r.branches << Branch.new(bran.name,
+                                       Commit.new(bran.commit.id,
+                                                  "#{bran.commit.author_name} <#{bran.commit.author_email}>",
+                                                  bran.commit.authored_date,
+                                                  r),
+                                       r)
+            end
+
+            r
+          end
+        end
+      end
+
+      def create_repository(name)
+        with_gitlab do
+          ::Gitlab.create_project(name, {
+            default_branch: 'master',
+            wiki_enabled: 0,
+            wall_enabled: 0,
+            issues_enabled: 0,
+            snippets_enabled: 0,
+            merge_requests_enabled: 0,
+            public: 0,
+            #user_id: @user_id
+          }).ssh_url_to_repo
+        end
+      end
+
+      private
+      def with_gitlab
         ::Gitlab.configure do |c|
           c.endpoint = @base
           c.private_token = @token
         end
 
         @user_id = ::Gitlab.user.id
-      end
 
-      def repositories
-        ::Gitlab.projects.auto_paginate.collect do |repo|
-          next if self.exclude.include? repo.name
-
-          r = Repository.new(repo.name, repo, repo.ssh_url_to_repo, self)
-
-          ::Gitlab.branches(repo.id).auto_paginate.each do |bran|
-            r.branches << Branch.new(bran.name,
-                                     Commit.new(bran.commit.id,
-                                                "#{bran.commit.author_name} <#{bran.commit.author_email}>",
-                                                bran.commit.authored_date,
-                                                r),
-                                     r)
-          end
-
-          r
-        end
-      end
-
-      def create_repository(name)
-        ::Gitlab.create_project(name, {
-          default_branch: 'master',
-          wiki_enabled: 0,
-          wall_enabled: 0,
-          issues_enabled: 0,
-          snippets_enabled: 0,
-          merge_requests_enabled: 0,
-          public: 0,
-          #user_id: @user_id
-        }).ssh_url_to_repo
+        yield
       end
     end
   end
