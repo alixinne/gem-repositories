36cbd75030c1944c08f832df0580b33329ccaa6e
Vincent Tavernier <vince.tavernier@gmail.com>
2019-05-04 00:36:20 +0200

    Make include_forks a set in GitHub provider

--- lib/repositories/hosts/github.rb
+++ lib/repositories/hosts/github.rb
@@ -12,7 +12,13 @@ module Repositories
     class Github < Base
       def initialize(config)
         super(config)
-        @include_forks = config.merge({ 'include_forks' => false })['include_forks']
+        i_forks = { 'include_forks' => [] }.merge(config)['include_forks']
+
+        if i_forks.is_a? Array
+          @include_forks = Set.new(i_forks)
+        else
+          @include_forks = if i_forks then true else false end
+        end
 
         @github = ::Github.new basic_auth: "#{@user}:#{@token}",
                                auto_pagination: true
@@ -22,11 +28,23 @@ module Repositories
         @include_forks
       end
 
+      def should_include_fork?(repo)
+        if repo.fork
+          if @include_forks == false
+            return false
+          end
+
+          return @include_forks.include? repo.name
+        end
+
+        true
+      end
+
       def repositories
         Enumerator.new do |yielder|
           @github.repos.list.each do |repo|
             next unless matches(repo.name)
-            next if include_forks? ^ repo.fork
+            next unless should_include_fork?(repo)
 
             yielder << Repository.new(repo.name, repo.description, repo, repo.ssh_url, repo.html_url, self) do |r, branches|
               @github.repos.branches.list(repo.owner.login, repo.name).each do |bran|
